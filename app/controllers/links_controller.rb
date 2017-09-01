31ff356d781da9959f879fb1906f8b1e73ee514f
buncis mamen <buncismamen@gmail.com>
2017-09-01 11:20:20 +0700

    ngerubah before action di links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+	acts_as_token_authentication_handler_for User, except: [:index, :show]
   before_action :set_kelas
   before_action :set_link, only: [:show, :update, :destroy]
   
